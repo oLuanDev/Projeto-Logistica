a64cfc08c73a1fd9dd083d4abdd774f4f1be8a79
oluaan <contato.luanpena@gmail.com>
2025-08-31 20:22:13 -0300

    deixando site ressponsivo

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 // Adicione seu JavaScript aqui, se necessário.
 // Por enquanto, os botões estão usando onclick diretamente no HTML.
 
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+
+menuToggle.addEventListener('click', () => {
+  // ativa a animação do X imediatamente
+  menuToggle.classList.toggle('active'); 
+  
+  // abre/fecha o menu com leve atraso
+  if (!menu.classList.contains('active')) {
+    setTimeout(() => {
+      menu.classList.add('active');
+    }, 100); // 100ms de delay
+  } else {
+    menu.classList.remove('active');
+  }
+});
+
+const menu = document.getElementById('menu');
+const menuToggle = document.getElementById('menu-toggle');
+
+// Garante que o menu esteja fechado quando a página é carregada ou restaurada do cache
+window.addEventListener('pageshow', () => {
+  menu.classList.remove('active');
+  menuToggle.classList.remove('active');
+});
 
